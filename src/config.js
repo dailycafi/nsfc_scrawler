4e1cb3a24a7053e1c40898082469475f728c716a
Fei Wen <feistar@gmail.com>
2025-01-03 08:46:33 +0800

    bug fix

--- src/config.js
+++ src/config.js
@@ -11,21 +11,23 @@ const FUND_TYPES = [
 
 // 年份和代理的固定对应关系
 const YEAR_PROXY_MAP = {
-    2009: { id: 'lrps-86384', ip: '36.25.243.5', port: '10859', location: '郑州市', isp: '移动' },
-    2010: { id: 'lrps-06377', ip: '36.25.243.5', port: '11889', location: '天津市', isp: '联通' },
-    2011: { id: 'lrps-06369', ip: '36.25.243.5', port: '10852', location: '济宁市', isp: '联通' },
-    2012: { id: 'lrps-06370', ip: '36.25.243.5', port: '11861', location: '邯郸市', isp: '联通' },
-    2013: { id: 'lrps-06371', ip: '36.25.243.5', port: '10853', location: '南昌市', isp: '电信' },
-    2014: { id: 'lrps-06372', ip: '36.25.243.5', port: '11865', location: '天津市', isp: '联通' },
-    2015: { id: 'lrps-26367', ip: '36.25.243.5', port: '11839', location: '运城市', isp: '联通' },
-    2016: { id: 'lrps-86383', ip: '36.25.243.5', port: '11921', location: '天津市', isp: '-' },
-    2017: { id: 'lrps-06374', ip: '36.25.243.5', port: '10854', location: '抚州市', isp: '电信' },
-    2018: { id: 'lrps-06375', ip: '36.25.243.5', port: '11885', location: '湛江市', isp: '移动' },
-    2019: { id: 'lrps-06376', ip: '36.25.243.5', port: '10855', location: '娄底市', isp: '电信' },
-    2020: { id: 'lrps-86380', ip: '36.25.243.5', port: '10857', location: '四平市', isp: '联通' },
-    2021: { id: 'lrps-86381', ip: '36.25.243.5', port: '10858', location: '黄山市', isp: '移动' },
-    2022: { id: 'lrps-86382', ip: '36.25.243.5', port: '11920', location: '邵阳市', isp: '联通' }
+    2011: { id: 'lrps-86384', ip: '36.25.243.5', port: '10859', location: '郑州市', isp: '移动' },
+    2012: { id: 'lrps-86380', ip: '36.25.243.5', port: '10857', location: '四平市', isp: '联通' },
+    // 2012: { id: 'lrps-06377', ip: '36.25.243.5', port: '11889', location: '天津市', isp: '联通' },
+    // 2013: { id: 'lrps-06369', ip: '36.25.243.5', port: '10852', location: '济宁市', isp: '联通' },
+    2013: { id: 'lrps-86381', ip: '36.151.192.236', port: '11723', location: '上海市', isp: '电信' },
+    // 2014: { id: 'lrps-06370', ip: '36.25.243.5', port: '11861', location: '邯郸市', isp: '联通' },
+    2014: { id: 'lrps-86382', ip: '36.25.243.5', port: '11920', location: '邵阳市', isp: '联通' },
+    2015: { id: 'lrps-06373', ip: '61.184.8.27', port: '11789', location: '东莞市', isp: '电信' },
+    2017: { id: 'lrps-06374', ip: '61.184.8.27', port: '11790', location: '天津市', isp: '联通' },
+    2018: { id: 'lrps-06375', ip: '61.184.8.27', port: '11791', location: '运城市', isp: '联通' },
+    2019: { id: 'lrps-06376', ip: '61.184.8.27', port: '11792', location: '河池市', isp: '-' },
+    2020: { id: 'lrps-06377', ip: '61.184.8.27', port: '11793', location: '抚州市', isp: '电信' },
+    2021: { id: 'lrps-06375', ip: '36.151.192.236', port: '11722', location: '淄博', isp: '电信' },
+    2022: { id: 'lrps-06376', ip: '36.151.192.236', port: '11720', location: '嘉兴市', isp: '电信' }
 };
+
+
 
 // 代理认证信息
 const PROXY_AUTH = {
